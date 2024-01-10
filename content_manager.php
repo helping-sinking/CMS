339715ee91467cfe3a03fcb33a05848e5aab59fa
Jenny V <153212464+helping-sinking@users.noreply.github.com>
2024-01-10 12:29:24 +0700

    Create content_manager.php

---
+++ content_manager.php
@@ -0,0 +1,12 @@
+<?php
+function getContent() {
+    // In a real scenario, this function might fetch content from a database
+    // For simplicity, using hardcoded content here
+    $content = array(
+        'latest_article' => 'This is the latest article.',
+        'featured_article' => 'Check out our featured article.'
+    );
+
+    return $content;
+}
+?>
